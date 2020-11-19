6eda696df9b783f73bc7f3a565c020e3d78e284b
Deno G <dguennoc@gmail.com>
2020-11-19 08:52:48 +0100

    Implémentation de la classe ServicePrevu pour accueillir les volumes horaires des CM, TD et TP

--- src/main/java/champollion/ServicePrevu.java
+++ src/main/java/champollion/ServicePrevu.java
@@ -1,6 +1,26 @@
 package champollion;
 
 public class ServicePrevu {
-	// TODO : implémenter cette classe
-
+    // TODO : implémenter cette classe
+    private int volumeCM;
+    private int volumeTD;
+    private int volumeTP;
+    
+    public ServicePrevu (int vCM, int vTD, int vTP){
+        this.volumeCM = vCM;
+        this.volumeTD = vTD;
+        this.volumeTP = vTP;
+    }
+    
+    public int getVolumeCM(){
+        return volumeCM;
+    }
+    
+    public int getVolumeTD(){
+        return volumeTD;
+    }
+    
+    public int getVolumeTP(){
+        return volumeTP;
+    }
 }
